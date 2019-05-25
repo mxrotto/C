8feb21462c4268da348b7c24d41411bf71bec3b3
Diessy <49379112+DiessySoares@users.noreply.github.com>
2019-05-25 15:55:40 -0300

    Update inverterString.c

--- Exercicios/inverterString.c
+++ Exercicios/inverterString.c
@@ -1,16 +1,18 @@
+//nao corrigido - espaco no final da frase faz com que a ordem perca o sentido!! 
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>					
 
 #define size 			100
 
 void revstring(char string[], char revstr[], int topo){
 	int i = 0, sizeT = topo;
-	for(topo; topo >= 0; topo--){								//decrementado topo
-		if(string[topo-1] == ' ' || topo == 0){					//se indice da _string_ - 1 eh igual a espaco ou indice esta no ponto inicial
-			while((topo + i) <= sizeT){							//enquanto indicie + i for menor que ultimo topo ou enquanto indicie + i for igual a espaco
-				revstr[topo + i] = string[sizeT - i];			//_revstr_ topo decrementado mais i recebe _string_ ultimo topo menos i
-				i++;													
+	for(topo; topo >= 0; topo--){					//decrementado topo
+		if(string[topo-1] == ' ' || topo == 0){			//se indice da _string_ - 1 eh igual a espaco ou indice esta no ponto inicial
+			while((topo + i) <= sizeT){			//enquanto indicie + i for menor que ultimo topo ou enquanto indicie + i for igual a espaco
+				revstr[topo + i] = string[sizeT - i];	//_revstr_ topo decrementado mais i recebe _string_ ultimo topo menos i
+				i++;												
 			}
 			sizeT = topo - 2;
 			revstr[topo - 1] = ' ';
